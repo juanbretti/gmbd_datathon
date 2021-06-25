b9c3a2a55a654e2fb8132c9a2f5f5090adec641d
Juan Pedro Bretti Mandarano <juan+github@bretti.com.ar>
2021-06-26 00:48:50 +0200

    Parson R2: 0.1525687788944724

--- make_model.py
+++ make_model.py
@@ -1,10 +1,35 @@
 # %%
 ## Libraries ----
+# General usage
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import helpers
 from joblib import dump, load
+
+# Custom library
+import helpers
+
+# Preprocessing
+from sklearn.preprocessing import StandardScaler
+
+# Plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import plot_confusion_matrix
+import seaborn as sns
+
+# Model
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+from skopt import BayesSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
+# Linear model
+import statsmodels.api as sm
+
 
 # %%
 ## Read dataframes ----
@@ -21,6 +46,12 @@ province_code = province_code[['Code provincia alpha', 'Code comunidad autónoma
 # %%
 ## Constants ----
 TARGET_PROVINCE = 'M'
+TRAIN_PERCENTAGE = 0.9
+TUNNING_PARAM_COMB = 10
+MAX_LAG = 21
+LAG_PANDEMIC = [1, 7, 14, MAX_LAG]
+LAG_OTHER = [0, 7, 14, MAX_LAG]
+SEED = 42
 
 # %%
 ## Prepare dataframes ----
@@ -43,9 +74,10 @@ df_casos_uci = df_casos_uci.merge(province_code, left_on='provincia_iso', right_
 df_casos_uci_age = pd.pivot_table(df_casos_uci, index=['fecha'], columns=['Code comunidad autónoma alpha', 'grupo_edad_merged'], values=['num_hosp', 'num_uci', 'num_def'], aggfunc=np.sum, fill_value=0)
 # Flatten column names and remove index
 df_casos_uci_age.columns = ['__'.join(x) for x in df_casos_uci_age.columns]
-
 df_casos_uci_age = df_casos_uci_age.reset_index()
-df_casos_uci_age = df_casos_uci_age.add_prefix('uci_age__')
+# Time shifting, minimum has to be `1`
+df_casos_uci_age_lagged = helpers.shift_timeseries_by_lags(df_casos_uci_age, fix_columns=['fecha'], lag_numbers=LAG_PANDEMIC)
+df_casos_uci_age_lagged = df_casos_uci_age_lagged.add_prefix('uci_age__')
 
 # %%
 ### Cases tested ----
@@ -54,7 +86,8 @@ df_casos_pruebas = pd.pivot_table(df_casos_pruebas, index=['fecha'], columns=['C
 
 df_casos_pruebas.columns = ['__'.join(x) for x in df_casos_pruebas.columns]
 df_casos_pruebas = df_casos_pruebas.reset_index()
-df_casos_pruebas = df_casos_pruebas.add_prefix('tests__')
+df_casos_pruebas_lagged = helpers.shift_timeseries_by_lags(df_casos_pruebas, fix_columns=['fecha'], lag_numbers=LAG_OTHER)
+df_casos_pruebas_lagged = df_casos_pruebas_lagged.add_prefix('tests__')
 
 # %%
 ### AEMET temperature ----
@@ -66,7 +99,8 @@ df_aemet_pivot = pd.pivot_table(df_aemet_pivot, index=['fecha'], columns=['Code 
 
 df_aemet_pivot.columns = ['__'.join(x) for x in df_aemet_pivot.columns]
 df_aemet_pivot = df_aemet_pivot.reset_index()
-df_aemet_pivot = df_aemet_pivot.add_prefix('aemet__')
+df_aemet_pivot_lagged = helpers.shift_timeseries_by_lags(df_aemet_pivot, fix_columns=['fecha'], lag_numbers=LAG_OTHER)
+df_aemet_pivot_lagged = df_aemet_pivot_lagged.add_prefix('aemet__')
 
 # %%
 ### Google Trends ----
@@ -75,49 +109,110 @@ df_googletrends_pivot = df_googletrends_pivot.pivot(index=['date'], columns=['ca
 
 df_googletrends_pivot.columns = ['__'.join(x) for x in df_googletrends_pivot.columns]
 df_googletrends_pivot = df_googletrends_pivot.reset_index()
-df_googletrends_pivot = df_googletrends_pivot.add_prefix('google_trends__')
+df_googletrends_pivot_lagged = helpers.shift_timeseries_by_lags(df_googletrends_pivot, fix_columns=['date'], lag_numbers=LAG_OTHER)
+df_googletrends_pivot_lagged = df_googletrends_pivot_lagged.add_prefix('google_trends__')
 
 # %%
 ### Movements to Madrid (`mitma`) ----
 filter_ = df_mitma['destino_province']==TARGET_PROVINCE
 df_mitma_filtered = df_mitma[filter_]
-df_mitma_filtered = df_mitma_filtered.drop('destino_province', axis=1)
-df_mitma_filtered = df_mitma_filtered.add_prefix('mitma__')
+df_mitma_filtered = df_mitma_filtered.drop(columns='destino_province')
+df_mitma_filtered_lagged = helpers.shift_timeseries_by_lags(df_mitma_filtered, fix_columns=['fecha'], lag_numbers=LAG_OTHER)
+df_mitma_filtered_lagged = df_mitma_filtered_lagged.add_prefix('mitma__')
 
 # %%
+## Merge all datasources ----
 df_merge = df_casos_uci_num_defunciones.copy()
 df_merge = df_merge.rename({'uci_defun__fecha': 'fecha'}, axis=1)
 
-def merge_df_to_add(df_to_add, date_column, df_merge):
-    df = df_merge.merge(df_to_add, left_on='fecha', right_on=date_column, how='outer')
-    df = df.drop(date_column, axis=1)
+def merge_df_to_add(df_merge, df_to_add, date_column):
+    df = df_merge.merge(df_to_add, left_on='fecha', right_on=date_column, how='inner')
+    df = df.drop(columns=date_column)
     return df
 
-df_merge = merge_df_to_add(df_casos_uci_age, 'uci_age__fecha', df_merge)
-df_merge = merge_df_to_add(df_casos_pruebas, 'tests__fecha', df_merge)
-df_merge = merge_df_to_add(df_aemet_pivot, 'aemet__fecha', df_merge)
-df_merge = merge_df_to_add(df_googletrends_pivot, 'google_trends__date', df_merge)
-df_merge = merge_df_to_add(df_mitma_filtered, 'mitma__fecha', df_merge)
+df_merge = merge_df_to_add(df_merge, df_casos_uci_age_lagged, 'uci_age__fecha')
+df_merge = merge_df_to_add(df_merge, df_casos_pruebas_lagged, 'tests__fecha')
+df_merge = merge_df_to_add(df_merge, df_aemet_pivot_lagged, 'aemet__fecha')
+df_merge = merge_df_to_add(df_merge, df_googletrends_pivot_lagged, 'google_trends__date')
+df_merge = merge_df_to_add(df_merge, df_mitma_filtered_lagged, 'mitma__fecha')
+
+# Fill `na` because of the `outer`
+df_merge = df_merge.fillna(df_merge.mean())
+# Check for `nan` or `null`
+# df_merge.isnull().sum().values.sum()
 
 # %%
-# TODO:
+## Feature engineering ----
+### Dates ----
+df_merge['fecha_year'] = df_merge['fecha'].dt.year
+df_merge['fecha_month'] = df_merge['fecha'].dt.month
+df_merge['fecha_day'] = df_merge['fecha'].dt.day
+df_merge['fecha_dayofweek'] = df_merge['fecha'].dt.dayofweek
+df_merge['fecha_dayofyear'] = df_merge['fecha'].dt.dayofyear
+df_merge['fecha_weekend'] = [(x in [5,6])*1 for x in df_merge['fecha_dayofweek']]
+df_merge = df_merge.drop(columns=['fecha'])
 
-# y = muertes
-# lag 7, 14 para todas
+# %%
+## Split dataset ----
+df_merge = df_merge.reset_index(drop=True)
+# 90% for the training
+split_ = int(df_merge.shape[0]*TRAIN_PERCENTAGE)
+# Split
+df_train = df_merge.iloc[MAX_LAG:split_, :]
+df_test = df_merge.iloc[split_:(split_+7), :]
 
-# mitma
-# ponerlo en columnas
-# lag de 7 y 14
+# X and y split
+X_train = df_train.drop(columns='uci_defun__num_def')
+y_train = df_train['uci_defun__num_def']
 
-# falta el censo
-# faltan las vacunas
+X_test = df_test.drop(columns='uci_defun__num_def')
+y_test = df_test['uci_defun__num_def']
 
-# hacer un standardscaler
+# %%
+## Model training ----
+### Quick model ----
+rfr = RandomForestRegressor(random_state=SEED, max_features='auto', n_estimators=1000, n_jobs=-1)
+rfr.fit(X_train, y_train)
+y_train_pred = rfr.predict(X_train)
+y_test_pred = rfr.predict(X_test)
 
-# Explain
-# * https://gist.github.com/aigera2007/567a6d34cefb30c7c6255c20e40f24fb/raw/9c9cb058d1e00533b7dd9dc8f0fd9d3ad03caabb/corr_matrix.py
-# * https://gist.github.com/aigera2007/789c2a996900bc88ea3f0d9494cbb9d0/raw/996fb3b7d0d46a151ab4486d841e9baeed30767b/roc_auc_curve.py
+# %%
+### Metrics ----
+print('** Train **')
+print(f'Parson R2: {metrics.r2_score(y_train, y_train_pred)}')
+print(f'Mean Squared Error: {metrics.mean_squared_error(y_train, y_train_pred)}')
+print(f'Mean Absolute Percengage Error: {metrics.mean_absolute_percentage_error(y_train, y_train_pred)}')
 
-# SHAP: 
-# * https://towardsdatascience.com/explain-any-models-with-the-shap-values-use-the-kernelexplainer-79de9464897a
-# * https://aigerimshopenova.medium.com/random-forest-classifier-and-shap-how-to-understand-your-customers-and-interpret-a-black-box-model-6166d86820d9
+print('** Test **')
+print(f'Parson R2: {metrics.r2_score(y_test, y_test_pred)}')
+print(f'Mean Squared Error: {metrics.mean_squared_error(y_test, y_test_pred)}')
+print(f'Mean Absolute Percengage Error: {metrics.mean_absolute_percentage_error(y_test, y_test_pred)}')
+
+# %%
+## Feature importance ----
+### Standard plot ----
+# https://stackoverflow.com/a/51520906/3780957
+feat_importances = pd.Series(rfr.feature_importances_, index=X_train.columns)
+feat_importances.nlargest(20).plot(kind='barh').invert_yaxis()
+
+# %%
+### Shap ----
+import shap
+explainer = shap.TreeExplainer(rfr)
+shap_values = explainer.shap_values(X_test)
+shap.summary_plot(shap_values, X_test, plot_type="bar")
+
+# %%
+
+# %%
+# TODO: lag 7, 14 para todas
+# TODO: Variables de pandemia, tienen que tener lag
+# TODO: falta el censo
+# TODO: faltan las vacunas
+# TODO: MOdelo lineal
+# TODO: Correlation
+# https://gist.github.com/aigera2007/567a6d34cefb30c7c6255c20e40f24fb/raw/9c9cb058d1e00533b7dd9dc8f0fd9d3ad03caabb/corr_matrix.py
+# TODO: SHAP 
+# https://towardsdatascience.com/explain-any-models-with-the-shap-values-use-the-kernelexplainer-79de9464897a
+# https://aigerimshopenova.medium.com/random-forest-classifier-and-shap-how-to-understand-your-customers-and-interpret-a-black-box-model-6166d86820d9
+# TODO: hacer un standardscaler
